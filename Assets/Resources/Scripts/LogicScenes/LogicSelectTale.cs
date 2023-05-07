b0cfb5bfacc4c61dd836c8bcb3e1cd34f270ec04
oxipusio <oxipusioxx12@gmail.com>
2023-05-07 13:08:36 -0500

    Add Map snow || lava

--- Assets/Resources/Scripts/LogicScenes/LogicSelectTale.cs
+++ Assets/Resources/Scripts/LogicScenes/LogicSelectTale.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
@@ -17,14 +18,24 @@ public class LogicSelectTale : MonoBehaviour
     private bool _actuallyShowLvl = false;
     public TMP_Text chronometerTmpText;
     public TMP_Text dayTmpText;
+    public GameObject parentWhatContainAllLight;
     public GameObject GoDoorLvlInConfirmation { get => goDoorLvlInConfirmation; set => goDoorLvlInConfirmation = value; }
-
+    private Color32 _colorAlertDownLevel;
+    private List<Color32> _colorOld;
+    private int _numberStage;
     private void Awake()
     {
         _instance = this;
         _hourToShowLvlAvaible = 18;
         _hourToShowLvlAvaibleFinish = 19;
         _whereIsLvl = "Hidden";
+        _colorOld = new List<Color32>();
+        _colorAlertDownLevel = new Color32(0xE5, 0x29, 0x1B, 0xFF);
+        foreach (Transform child in parentWhatContainAllLight.transform)
+        {
+            _colorOld.Add(child.GetComponent<Light>().color);
+        }
+        _numberStage = 1;
     }
     public static LogicSelectTale GetInstance()
     {
@@ -39,18 +50,18 @@ public class LogicSelectTale : MonoBehaviour
     void Update()
     {
         chronometerTmpText.text = theChronometerObject.GetInformationHour();
-        dayTmpText.text  = theChronometerObject.DayText;
-        if(_hourToShowLvlAvaible == theChronometerObject.HourActual && !_actuallyShowLvl)
+        dayTmpText.text = theChronometerObject.DayText;
+        if (_hourToShowLvlAvaible == theChronometerObject.HourActual && !_actuallyShowLvl)
         {
-            StartDownLvl();    
+            StartDownLvl();
             _actuallyShowLvl = true;
             _whereIsLvl = "Show";
         }
 
-        if(_hourToShowLvlAvaibleFinish == theChronometerObject.HourActual && _actuallyShowLvl)
+        if (_hourToShowLvlAvaibleFinish == theChronometerObject.HourActual && _actuallyShowLvl)
         {
             _actuallyShowLvl = false;
-            if(_whereIsLvl == "Show")
+            if (_whereIsLvl == "Show")
             {
                 StartHiddenLvl();
                 _whereIsLvl = "Hidden";
@@ -67,10 +78,15 @@ public class LogicSelectTale : MonoBehaviour
 
     public IEnumerator ProcessDown(GameObject go)
     {
+        foreach (Transform child in parentWhatContainAllLight.transform)
+        {
+            child.GetComponent<Light>().color = _colorAlertDownLevel;
+        }
+
         Animator _myAnim = go.GetComponent<Animator>();
         _myAnim.SetBool("DownLvl", true);
         _myAnim.SetBool("ReverseDownLvl", false);
-        
+
         while (!_myAnim.GetCurrentAnimatorStateInfo(0).IsName("DownLevel"))
         {
             yield return null;
@@ -91,6 +107,12 @@ public class LogicSelectTale : MonoBehaviour
 
     public IEnumerator ProcessHidden(GameObject go)
     {
+        int i = 0;
+        foreach (Transform child in parentWhatContainAllLight.transform)
+        {
+            child.GetComponent<Light>().color = _colorOld[i];
+            i++;
+        }
         Animator _myAnim = go.GetComponent<Animator>();
         _myAnim.SetBool("DownLvl", false);
         _myAnim.SetBool("ReverseDownLvl", true);
@@ -109,9 +131,56 @@ public class LogicSelectTale : MonoBehaviour
     public void ContinueOpenDoor()
     {
         goConfirmationLvl.SetActive(false);
-        StartCoroutine(goDoorLvlInConfirmation.GetComponent<DoorInteractuable>().ProcessOpen());        
+        StartCoroutine(goDoorLvlInConfirmation.GetComponent<DoorInteractuable>().ProcessOpen());
     }
 
+    // public void StagesGame()
+    // {
+    //     switch()
+    //     {
 
+    //     }
+    //     // Stage 1
+    //     // _numberStage
+    //     // Stage 2
+    //     // Stage 3
+    //     // Stage 4
+    //     // Stage 5
+    //     // Stage Final
+    // }
+
+    public void MapByStages(int numberStage)
+    {
+        int valueRandom = 0;
+        int quantityMapRandom = 0;
+        int quantityTotalDoorInMap = 3;
+        int quantityMinTotalDoorInMap = 1;
+        switch (numberStage)
+        {
+            // SceneManager.LoadScene(4,LoadSceneMode.Single);
+            case 1:
+                valueRandom = Random.Range(quantityMinTotalDoorInMap, quantityTotalDoorInMap + 1);
+
+                // SceneManager.LoadScene(4,LoadSceneMode.Single);
+                break;
+            case 2:
+                // SceneManager.LoadScene(4,LoadSceneMode.Single);
+                break;
+            case 3:
+                // SceneManager.LoadScene(4,LoadSceneMode.Single);
+                break;
+            case 4:
+                // SceneManager.LoadScene(4,LoadSceneMode.Single);
+                break;
+            case 5:
+                // SceneManager.LoadScene(4,LoadSceneMode.Single);
+                break;
+            case 6:
+                // SceneManager.LoadScene(4,LoadSceneMode.Single);
+                break;
+            default:
+                break;
+        }
+    }
 
 }
